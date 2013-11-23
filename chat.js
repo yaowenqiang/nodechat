de351281b29f6de6b5bfa5d7a80a31473eab3963
jack yao <yaowenqiang111@163.com>
2013-11-24 03:40:30 +0800

    add the online user list

--- chat.js
+++ chat.js
@@ -7,41 +7,64 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 var messages = [];
-var storeMessage = function(name,data){
+var storeMessage = function (name, data) {
     //messages.push({name:name,data:data});
     //if(messages.length >9){
-        //messages.shift();
+    //messages.shift();
     //}
-    var message = JSON.stringify({name:name,data:data});
-    redisClient.lpush('messages',message,function(err,response){
-        redisClient.ltrim('messages',0,10);
+    var message = JSON.stringify({
+        name: name,
+        data: data
+    });
+//    console.log('store messages.');
+    redisClient.lpush('messages', message, function (err, response) {
+        redisClient.ltrim('messages', 0, 10);
     })
 };
 server.listen(8080);
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/index.html');
 });
-//var io = socket.listen(app);
-io.sockets.on('connection',function(client){
-    //console.log('client connected...');
-    //client.emit('messages',{hello:'world'});
-    client.on('join',function(name){
-        client.set('nickname',name);
+//var io = socket.listen(app);
+io.sockets.on('connection', function (client) {
+//        client.get('nickname', function (err, name) {
+//            if(!name){
+//                console.log('the user does not oofer a name');
+//                client.emit('askname');
+//                return;
+//            }
+//        });
+    client.on('join', function (name) {
+        client.broadcast.emit('addchatter',name);
+        redisClient.smembers('chatters',function(err,names){
+            names.forEach(function(name){
+                console.log(name);
+                client.emit('addchatter',name);
+            });
+        });
+        redisClient.sadd('chatters',name);
+        client.set('nickname', name);
         //messages.forEach(function(message){
-            //client.emit('messages',message.name+':'+message.data);
+        //client.emit('messages',message.name+':'+message.data);
         //});
-        redisClient.lrange('messages',0,-1,function(err,messages){
+        redisClient.lrange('messages', 0, -1, function (err, messages) {
             messages = messages.reverse();
-            messages.forEach(function(message){
+            messages.forEach(function (message) {
                 message = JSON.parse(message);
-                client.emit('messages',message.name+':'+message.data);
+                client.emit('messages', message.name + ':' + message.data);
             });
         })
     })
-    client.on('messages',function(data){
+    client.on('messages', function (data) {
+        client.get('nickname', function (err, name) {
+            storeMessage(name, data);
+            client.broadcast.emit('messages', name + ':' + data);
+        });
+    });
+    client.on('disconnect',function(name){
         client.get('nickname',function(err,name){
-            storeMessage(name,data);
-            client.broadcast.emit('messages',name + ':'+data);
+            client.broadcast.emit('removechatter',name);
+            redisClient.srem('chatters',name);
         });
     });
 });
